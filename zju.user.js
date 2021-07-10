3c6453b6934edb9d89f94a2fc6d3460621a8be9c
Di Weng <mystery.wd@gmail.com>
2021-07-11 01:51:15 +0800

    Update zju.user.js

--- zju.user.js
+++ zju.user.js
@@ -1,46 +1,44 @@
 // ==UserScript==
 // @name         Zhejiang University Beancount Exporter
 // @namespace    https://github.com/w1ndy/beancount-helpers
-// @version      1.0
+// @version      1.1
 // @description  Extract and export Zhejiang University transactions to Beancount
 // @author       w1ndy
-// @match        http://ecardhall.zju.edu.cn:808/Page/Page
+// @match        http://mapp.zju.edu.cn/_web/_customizes/pc_logistics/card/oneCardSolution.html
 // @grant        GM_setClipboard
 // ==/UserScript==
 
 function mount() {
-    const btn = document.createElement('a');
-    btn.className = "button";
+    const btn = document.createElement('button');
+    btn.className = "layui-btn";
+    btn.type = "button";
     btn.onclick = function () {
-        let balanceChecked = false;
-        const rows = document.querySelector('.datagrid-view2 .datagrid-body').querySelectorAll('tr');
+        const rows = document.querySelector('#table-body-custom').querySelectorAll('tr');
 
         const transactions = [];
         for (const row of rows) {
-            const date = row.querySelector('td[field="OCCTIME"]').innerText.split(' ')[0];
-            const name = row.querySelector('td[field="MERCNAME"]').innerText;
-            const amount = parseFloat(row.querySelector('td[field="TRANAMT"]').innerText);
-            if (!balanceChecked) {
-                const bal = parseFloat(row.querySelector('td[field="CARDBAL"]').innerText);
-                transactions.unshift(`${(new Date((new Date(date)).getTime() + 86400000)).toISOString().split('T')[0]} balance Assets: ${bal} CNY`);
-                balanceChecked = true;
-            }
+            const cells = row.querySelectorAll('td');
+            const date = cells[0].innerHTML;
+            const name = cells[1].innerText;
+            const amount = cells[3].innerText;
 
-            let transaction = `${date} * "${name}"\n`;
-            transaction += `    Assets: ${amount >= 0 ? '+' : ''}${amount.toFixed(2)} CNY\n`;
-            transaction += `    ${amount >= 0 ? 'Income' : 'Expenses'}:`;
+            let transaction = `${date} * "${name || '充值'}"\n`;
+            transaction += `    Assets: ${name ? '-' : '+'}${amount} CNY\n`;
+            transaction += `    ${name ? 'Expenses' : 'Assets'}:`;
             transactions.unshift(transaction)
         }
 
+        transactions.push(`${new Date().toISOString().split('T')[0]} balance Assets: ${document.querySelector('#card-balance-num').innerHTML} CNY\n`);
+
         GM_setClipboard(transactions.join('\n\n'));
-        btn.innerHTML = '<i></i><span>复制成功！</span><s></s>';
+        btn.innerHTML = '复制成功！';
     }
-    btn.innerHTML = '<i></i><span>导出本页为Beancount格式</span><s></s>';
-    document.querySelector('.box dd:last-of-type').appendChild(btn);
+    btn.innerHTML = '导出本页为Beancount格式';
+    document.querySelector('.form-group .layui-inline').appendChild(btn);
 }
 
 function tryMount() {
-    if (!document.querySelector('.searchdiv')) {
+    if (!document.querySelector('.form-group .layui-inline')) {
         setTimeout(tryMount, 500);
     } else {
         mount();
